4d98c5ab5838e08283f3bb83600baf6552b78cf9
Taro.Benkyo <highflat6026@yahoo.co.jp>
2017-10-22 14:37:03 +0000

    delete redirect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
     if logged_in?
       @user = current_user
@@ -46,7 +45,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
   
   private
